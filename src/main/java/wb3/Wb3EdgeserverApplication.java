3d2cb5cddd08c370460f50ae1d9a884f957ef64d
William Bensing <WBensing@pods.com>
2017-02-25 18:42:00 -0500

    update configuration, added Bean for

--- src/main/java/wb3/Wb3EdgeserverApplication.java
+++ src/main/java/wb3/Wb3EdgeserverApplication.java
@@ -1,9 +1,18 @@
 package wb3;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.MessageFormat;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,5 +21,48 @@ public class Wb3EdgeserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Wb3EdgeserverApplication.class, args);
+	}
+
+	@Value("${server.port}")
+	String port;
+
+	@Bean
+	/**
+	 * Dynamically assign the hosts IP address to this instances of the Eureka Client
+	 *
+	 * @param  inetUtils  Java Networks Utilities
+	 * @return	An instance of a EurekaInstanceConfigBean with dynamically configured IP address
+	 */
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils){
+		EurekaInstanceConfigBean config = new EurekaInstanceConfigBean(inetUtils);
+
+		String ip = null;
+		String urlPattern = "http://{0}:{1}{2}";
+
+		try {
+			// Get the IP address of the current host using Java Network Utils
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+
+		config.setIpAddress(ip);
+		config.setPreferIpAddress(true);
+
+		// Build the Status Page URL specific to this instances IP Address
+		String statusPageUrl = MessageFormat.format(urlPattern,
+				config.getIpAddress(),
+				port,
+				config.getStatusPageUrlPath());
+		config.setStatusPageUrl(statusPageUrl);
+
+		// Build the Health Page URL specific to this instances IP Address
+		String healthPageUrl = MessageFormat.format(urlPattern,
+				config.getIpAddress(),
+				port,
+				config.getHealthCheckUrlPath());
+		config.setHealthCheckUrl(healthPageUrl);
+
+		return config;
 	}
 }
